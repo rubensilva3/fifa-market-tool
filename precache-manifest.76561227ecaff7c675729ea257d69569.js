40fbf2d0968ea861c5fd0bed77930c7af2a616f4
rubensilva3 <rubentiago3@gmail.com>
2019-01-20 17:54:48 +0000

    Updates

---
+++ precache-manifest.76561227ecaff7c675729ea257d69569.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cfb82467091a9d867da",
+    "url": "/fifa-market-tool/static/js/runtime~main.7cfb8246.js"
+  },
+  {
+    "revision": "ec6f21a71d04f99cf3df",
+    "url": "/fifa-market-tool/static/js/main.ec6f21a7.chunk.js"
+  },
+  {
+    "revision": "71f619240b74c1f0fa5c",
+    "url": "/fifa-market-tool/static/js/1.71f61924.chunk.js"
+  },
+  {
+    "revision": "eb2d47dc108bab26aed9ee908dcf9cf2",
+    "url": "/fifa-market-tool/index.html"
+  }
+];
